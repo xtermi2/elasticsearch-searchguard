30c6ea148824d0b4472b5c4c7456b7d6749a3977
Andreas Keefer <andreas.keefer@senacor.com>
2019-05-11 00:47:44 +0200

    update to 7.0.0

--- src/main/resources/bin/initSearchguard.sh
+++ src/main/resources/bin/initSearchguard.sh
@@ -7,6 +7,10 @@
 chmod -R 0770 $SG_CERT_DIR
 chown 1000:0 $SG_CERT_DIR
 
+# set JAVA_HOME and java to PATH
+export JAVA_HOME=/usr/share/elasticsearch/jdk
+export PATH=${PATH}:${JAVA_HOME}/bin
+
 /usr/local/bin/wait_until_started.sh
 
 /usr/local/bin/sgusers.sh
--- src/main/resources/bin/sgadmin.sh
+++ src/main/resources/bin/sgadmin.sh
@@ -2,9 +2,9 @@
 
 echo "configure searchguard via sgadmin.sh ..."
 
-chmod +x /usr/share/elasticsearch/plugins/search-guard-6/tools/sgadmin.sh
+chmod +x /usr/share/elasticsearch/plugins/search-guard-7/tools/sgadmin.sh
 
-/usr/share/elasticsearch/plugins/search-guard-6/tools/sgadmin.sh \
+/usr/share/elasticsearch/plugins/search-guard-7/tools/sgadmin.sh \
 -cacert ${SG_CERT_DIR}/${ROOT_CA} \
 -cert ${SG_CERT_DIR}/${ADMIN_PEM} \
 -key ${SG_CERT_DIR}/${ADMIN_KEY} \
